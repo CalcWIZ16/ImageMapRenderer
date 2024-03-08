1a4be4d5d3a48e898b8da08629fbf18ab30cf779
Luke Saccento <76127645+CalcWIZ16@users.noreply.github.com>
2024-03-07 19:24:52 -0800

    support for optimal side length calculations

--- src/main/java/net/lukesmp/imagemaprenderer/CustomMapArray.java
+++ src/main/java/net/lukesmp/imagemaprenderer/CustomMapArray.java
@@ -1,32 +1,65 @@
 package net.lukesmp.imagemaprenderer;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URL;
-
 
 public class CustomMapArray {
 
-    private int width;
-    private int height;
+    private int numMapsWidth;
+    private int numMapsHeight;
+    private int mapWidth;
+    private int mapHeight;
     private BufferedImage image;
-    private BufferedImage[][] images;
+    private BufferedImage[] images;
 
-    public CustomMapArray(int width, int height, BufferedImage image) {
-        this.width = width;
-        this.height = height;
+    /**
+     * Constructor for CustomMapArray
+     * @param numMapsWidth width (in maps) of the array (if -1, the optimal width will be calculated)
+     * @param numMapsHeight height (in maps) of the array (if -1, the optimal height will be calculated)
+     * @param image image to make up the array
+     */
+    public CustomMapArray(int numMapsWidth, int numMapsHeight, BufferedImage image) {
         this.image = image;
+        if (numMapsWidth == -1 && numMapsHeight == -1) {
+                this.numMapsWidth = 1;
+                this.numMapsHeight = 1;
+                this.mapWidth = image.getWidth();
+                this.mapHeight = image.getHeight();
+        } else if (numMapsWidth == -1) {
+            this.mapWidth = image.getWidth() / numMapsHeight;
+            this.mapHeight = image.getHeight() / numMapsHeight;
+
+            this.numMapsWidth = image.getWidth() / mapWidth;
+            this.numMapsHeight = numMapsHeight;
+        } else if (numMapsHeight == -1) {
+            this.mapWidth = image.getWidth() / numMapsWidth;
+            this.mapHeight = image.getHeight() / numMapsWidth;
+
+            this.numMapsWidth = numMapsWidth;
+            this.numMapsHeight = image.getHeight() / mapHeight;
+
+        } else {
+            this.numMapsWidth = numMapsWidth;
+            this.numMapsHeight = numMapsHeight;
+            this.mapWidth = image.getWidth() / numMapsWidth;
+            this.mapHeight = image.getHeight() / numMapsHeight;
+        }
     }
 
-    public void splitImage() {
-        images = new BufferedImage[width][height];
-        int mapWidth = image.getWidth() / width;
-        int mapHeight = image.getHeight() / height;
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                images[x][y] = image.getSubimage(x * mapWidth, y * mapHeight, mapWidth, mapHeight);
+    public BufferedImage[] splitImage() {
+        images = new BufferedImage[numMapsWidth*numMapsHeight];
+        for (int i = 0; i < numMapsWidth; i++) {
+            for (int j = 0; j < numMapsHeight; j++) {
+                images[i + j * numMapsWidth] = image.getSubimage(i * mapWidth, j * mapHeight, mapWidth, mapHeight);
             }
         }
+        return images;
+    }
+
+    public int getNumMapsWidth() {
+        return numMapsWidth;
+    }
+
+    public int getNumMapsHeight() {
+        return numMapsHeight;
     }
 }
--- src/main/java/net/lukesmp/imagemaprenderer/ImageMapRenderer.java
+++ src/main/java/net/lukesmp/imagemaprenderer/ImageMapRenderer.java
@@ -1,17 +1,15 @@
 package net.lukesmp.imagemaprenderer;
 
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.yaml.snakeyaml.Yaml;
-import sun.security.krb5.Config;
 
 import java.io.File;
 import java.io.IOException;
 
-public final class ImageMapRenderer extends JavaPlugin {
+public final class ImageMapRenderer extends JavaPlugin implements Listener {
 
     public static JavaPlugin plugin = null;
 
@@ -19,7 +17,7 @@ public final class ImageMapRenderer extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
-        Bukkit.getServer().getPluginManager().registerEvents(, ImageMapRenderer.getPlugin(ImageMapRenderer.class));
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
         ConfigUpdate();
         this.getCommand("map").setExecutor(new MapCommand());
         this.getCommand("map").setTabCompleter(new TabComplete());
