cb75dc056099970087d42196d122c2ca15bbba11
Luke Saccento <76127645+CalcWIZ16@users.noreply.github.com>
2024-03-07 18:21:03 -0800

    implemented splitImage() method

--- src/main/java/net/lukesmp/imagemaprenderer/CustomMapArray.java
+++ src/main/java/net/lukesmp/imagemaprenderer/CustomMapArray.java
@@ -1,6 +1,9 @@
 package net.lukesmp.imagemaprenderer;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
 
 public class CustomMapArray {
@@ -13,8 +16,17 @@ public class CustomMapArray {
     public CustomMapArray(int width, int height, BufferedImage image) {
         this.width = width;
         this.height = height;
-
+        this.image = image;
     }
 
-
+    public void splitImage() {
+        images = new BufferedImage[width][height];
+        int mapWidth = image.getWidth() / width;
+        int mapHeight = image.getHeight() / height;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                images[x][y] = image.getSubimage(x * mapWidth, y * mapHeight, mapWidth, mapHeight);
+            }
+        }
+    }
 }
