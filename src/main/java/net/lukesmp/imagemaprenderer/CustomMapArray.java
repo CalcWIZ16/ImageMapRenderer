c8eacd827ec7602c6586813d5c2418e46e7f5503
Luke Saccento <76127645+CalcWIZ16@users.noreply.github.com>
2024-05-23 00:05:07 -0700

    optimized CustomMapArray constructor

--- src/main/java/net/lukesmp/imagemaprenderer/CustomMapArray.java
+++ src/main/java/net/lukesmp/imagemaprenderer/CustomMapArray.java
@@ -16,8 +16,6 @@ public class CustomMapArray {
 
     private final int numMapsWidth;
     private final int numMapsHeight;
-    private final int mapWidth;
-    private final int mapHeight;
     private BufferedImage image;
     private CustomMap[] maps;
 
@@ -31,34 +29,24 @@ public class CustomMapArray {
         if (numMapsWidth == -1 && numMapsHeight == -1) {
             this.numMapsWidth = 1;
             this.numMapsHeight = 1;
-
-            this.image = resize(image, 128, 128);
         } else if (numMapsWidth == -1) {
-            this.mapWidth = image.getWidth() / numMapsHeight;
-            this.mapHeight = image.getHeight() / numMapsHeight;
-
-            this.numMapsWidth = image.getWidth() / mapWidth;
             this.numMapsHeight = numMapsHeight;
+            this.numMapsWidth = image.getWidth() / (image.getWidth()/numMapsHeight);
         } else if (numMapsHeight == -1) {
-            this.mapWidth = image.getWidth() / numMapsWidth;
-            this.mapHeight = image.getHeight() / numMapsWidth;
-
             this.numMapsWidth = numMapsWidth;
-            this.numMapsHeight = image.getHeight() / mapHeight;
-
+            this.numMapsHeight = image.getHeight() / (image.getHeight() / numMapsWidth);
         } else {
             this.numMapsWidth = numMapsWidth;
             this.numMapsHeight = numMapsHeight;
-            this.mapWidth = image.getWidth() / numMapsWidth;
-            this.mapHeight = image.getHeight() / numMapsHeight;
         }
+        this.image = resize(image, 128*numMapsWidth, 128*numMapsHeight);
     }
 
     public void createMaps() throws IOException {
         maps = new CustomMap[numMapsWidth*numMapsHeight];
         for (int i = 0; i < numMapsWidth; i++) {
             for (int j = 0; j < numMapsHeight; j++) {
-                maps[i + j * numMapsWidth] = new CustomMap(x * 128, y * 128, 128, 128));
+                maps[i + j * numMapsWidth] = new CustomMap(image.getSubimage(i*128, j*128, 128, 128));
             }
         }
     }
